93079879cd6dddfa74526d315dce5efb839c5711
LudovicGouverneur <ludovic.gouverneur@gmail.com>
2018-08-09 15:42:06 +0200

    Update addass-num-cross-str.js

--- addass-num-cross-str.js
+++ addass-num-cross-str.js
@@ -19,11 +19,13 @@ console.assert(str_str === actual, "fail: str_str");
 str_1 = "1";
 let str_num;
 { // str_1 += 1;
-
+  let Step_1 = String(1);
+  str_num = str_1 + Step_1;
 };
 actual = "1";
 actual += 1;
 console.assert(str_num === actual, "fail: str_num");
+
 
 let num_1 = 1;
 let num_str;
